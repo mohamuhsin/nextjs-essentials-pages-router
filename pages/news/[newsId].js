81a41efee5d1863a558e5c5e60884631fd3b4f16
mohamuhsin <muhsinohoho@outlook.com>
2024-11-23 23:16:56 +0300

    extracting dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,3 +1,9 @@
+import { useRouter } from "next/router";
+
 export default function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+    // send request to a backend API to fetch news item with the news ID.
     return <h1>The Detail Page</h1>;
 }
