ff44476f3bf417888c46042be4330a2ee578318f
mohamuhsin <muhsinohoho@outlook.com>
2024-11-23 23:29:06 +0300

    linking btn pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
-// our domain /
+// our domain/
+
+import Link from "next/link";
 
 export default function HomePage() {
-    return <h1>Home Page</h1>;
+    return (
+        <>
+            <h1>Home Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">
+                        NextJs is great framework
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/news/something-else">Something else</Link>
+                </li>
+            </ul>
+        </>
+    );
 }
